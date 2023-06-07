f9904b8b82057c8097ce2245dbf7c8d64e7c946b
MaksimSvyatov <masvjatov@gmail.com>
2023-06-07 12:20:44 +0400

    Add automatically creating user profiles

--- social/musker/admin.py
+++ social/musker/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile
 
-# Register your models here.
+# Unregister Group
+admin.site.unregister(Group)
+
+# Extend User Class
+
+# Mix Profile info into User infj
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Just display username fields in admin page
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+# Unregister initial user
+admin.site.unregister(User)
+
+# Register user  anf Profile
+admin.site.register(User, UserAdmin)
+# admin.site.register(Profile)
